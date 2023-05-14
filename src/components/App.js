e358736b6813d6c888c9bf0777e68521e827e59c
Kapil Chaudhary <kc685838@gmail.com>
2023-05-14 12:57:18 +0530

    submitting code-Sun May 14 2023 12:57:16 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -30,18 +30,18 @@ function App() {
         <button onClick={handleClick} name="/" className="operator">/</button>
         <button onClick={handleClick} name="*" className="operator">*</button>
         <button onClick={handleClick} name="-" className="operator">-</button>
-        <button onClick={handleClick} name="7">7</button>
-        <button onClick={handleClick} name="8">8</button>
-        <button onClick={handleClick} name="9">9</button>
+        <button onClick={handleClick} id='btn-7' name="7">7</button>
+        <button onClick={handleClick} id='btn-8' name="8">8</button>
+        <button onClick={handleClick} id='btn-9' name="9">9</button>
         <button onClick={handleClick} name="+" className="operator" id='plus' aria-rowspan={2}>+</button>
-        <button onClick={handleClick} name="4">4</button>
-        <button onClick={handleClick} name="5">5</button>
-        <button onClick={handleClick} name="6">6</button>
-        <button onClick={handleClick} name="1">1</button>
-        <button onClick={handleClick} name="2">2</button>
-        <button onClick={handleClick} name="3">3</button>
+        <button onClick={handleClick} id='btn-4' name="4">4</button>
+        <button onClick={handleClick} id='btn-5' name="5">5</button>
+        <button onClick={handleClick} id='btn-6' name="6">6</button>
+        <button onClick={handleClick} id='btn-1' name="1">1</button>
+        <button onClick={handleClick} id='btn-2' name="2">2</button>
+        <button onClick={handleClick} id='btn-3' name="3">3</button>
         <button onClick={calculate} id="result">=</button>
-        <button onClick={handleClick} name="0">0</button>
+        <button onClick={handleClick} id='btn-0' name="0">0</button>
         <button onClick={handleClick} name=".">.</button>
       </div>
     </div>
